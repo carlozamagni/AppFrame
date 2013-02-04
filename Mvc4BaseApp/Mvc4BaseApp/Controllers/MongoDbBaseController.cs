8923cbd01611e906899acb4e44ad242e66cf5f1c
Carlo Zamagni <carlozamagni@gmail.com>
2013-02-04 09:48:21 +0100

    skeleton

---
+++ Mvc4BaseApp/Mvc4BaseApp/Controllers/MongoDbBaseController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+
+namespace Mvc4BaseApp.Controllers
+{
+    public class MongoDbBaseController : Controller
+    {
+        public MongoDatabase Database
+        {
+            get
+            {
+                return MongoDatabase.Create(GetMongoDbConnectionString());
+            }
+        }
+
+        public MongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return Database.GetCollection<T>(collectionName);
+        }
+
+        private string GetMongoDbConnectionString()
+        {
+            return ConfigurationManager.AppSettings.Get("MONGODB_URL");
+        }
+    }
+}
---
+++ Mvc4BaseApp/Mvc4BaseApp/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using Mvc4BaseApp.Installer;
+
+namespace Mvc4BaseApp
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static IWindsorContainer _container;
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+
+            StartIocContainer();
+
+            BootstrapSupport.BootstrapBundleConfig.RegisterBundles(System.Web.Optimization.BundleTable.Bundles);
+            BootstrapMvcSample.ExampleLayoutsRouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_End()
+        {
+            _container.Dispose();
+        }
+
+
+        private static void StartIocContainer()
+        {
+            _container = new WindsorContainer()
+                .Install(FromAssembly.This());
+            var controllerFactory = new WindsorControllerFactory(_container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+    }
+}
